ec0dcc319c36a48353bb815f4dfa682a501d7213
shiyubei <shiyubei@qince.com>
2024-04-23 19:34:49 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Button} from "antd";
+const App = () => {
+  return (
+    <div className="App">
+        <Button>antd</Button>
+    </div>
+  );
+}
+
+export default App;
