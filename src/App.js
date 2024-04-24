e436b031d6fe0bc9c02248ba6a65e2a9676d64c0
shiyubei <shiyubei@qince.com>
2024-04-24 15:37:47 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import {Button} from "antd";
-const App = () => {
+// MyComponent.js
+
+import React, { useState } from "react";
+
+const MyComponent = () => {
+  const [result, setResult] = useState(null);
+
+  const handleWorkerMessage = (event) => {
+    // 接收来自工作线程的消息并更新状态
+    setResult(event.data);
+  };
+
+  const runWorker = () => {
+    // 创建一个新的 Worker 实例
+    const worker = new Worker(new URL("./worker.js", import.meta.url));
+
+    worker.onmessage = handleWorkerMessage; // 设置消息处理函数
+    // 向工作线程发送消息（这里可以是任何数据）
+    worker.postMessage("start");
+  };
+
   return (
-    <div className="App">
-        <Button>antd</Button>
+    <div>
+      <button onClick={runWorker}>Run Worker</button>
+      {result && <p>Result: {result}</p>}
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
